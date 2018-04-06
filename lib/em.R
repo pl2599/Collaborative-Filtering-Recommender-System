bce0bb5cd9c280f8c03d80f17979b96571f5b7cc
jkafrouni <j.kafrouni@gmail.com>
2018-04-06 13:09:46 -0400

    made em compatible with both dataset

--- lib/em.R
+++ lib/em.R
@@ -1,24 +1,30 @@
 
 # EM algorithm:
-em <- function(UI_matrix, C, epsilon, max_steps=100){
+em <- function(UI_matrix, C, epsilon, max_steps=100, dataset='movies'){
   
+  if (dataset=='movies'){
+    possible_ratings = (1:6)
+  } else if (dataset=='MS'){
+    possible_ratings = (0:1)
+  }
   n_users <- dim(UI_matrix)[1]
   n_items <- dim(UI_matrix)[2]
   step <- 0
+  K <- length(possible_ratings)
   prob_class <- matrix(1/C, nrow=C)
-  prob_rating <- array(1/6, dim=c(C, n_items, 6))
+  prob_rating <- array(1/K, dim=c(C, n_items, K))
   assignment <- matrix(NA, nrow=n_users, ncol=C)
   
   while (TRUE){
     step <- step + 1
     # E:
-    new_assignment <- update_assignment(UI_matrix, C, assignment, prob_class, prob_rating)
+    new_assignment <- update_assignment(UI_matrix, C, assignment, prob_class, prob_rating, K)
     
     # M:
     new_prob_class <- update_prob_class(UI_matrix, C, prob_class, new_assignment)
-    new_prob_rating <- update_prob_rating(UI_matrix, C, prob_rating, new_assignment)
-    
-    if ((step > 1) & (norm(new_prob_class - prob_class) < epsilon)){
+    new_prob_rating <- update_prob_rating(UI_matrix, C, prob_rating, new_assignment, possible_ratings)
+
+    if ((step > 1) & (norm(new_prob_class - prob_class) < epsilon) & (norm(as.matrix(new_prob_rating - prob_rating)) < epsilon)){
       # convergence: we check if prob_class and prob_rating converge
       # TODO: maybe checking for convergence of likelihood would be better ?
       print(paste0('converged in ', step, ' steps.'))
@@ -37,13 +43,15 @@ em <- function(UI_matrix, C, epsilon, max_steps=100){
 
 
 # Expectation step:
-update_assignment <- function(UI_matrix, C, assignment, prob_class, prob_rating){
+update_assignment <- function(UI_matrix, C, assignment, prob_class, prob_rating, K){
   # trick: prob_rating contains floats between 0 and 1
   # since we do a products of a lot of these floats, we'll underflow at some point
   # to fix this scale prob_rating (to mean=1) initially
   # since we're taking ratios eventually this is still correct.
   n_users <- dim(UI_matrix)[1]
   prob_rating <- prob_rating/mean(prob_rating)
+  k_fix <- ifelse(K==2, 1, 0) # for MS, possible ratings are 0 or 1, but prob_rating is indexed starting 1, so shift if necessary
+
   for (i in 1:n_users){
     movies_rated_by_i <- which(!is.na(UI_matrix[i,]))
     ratings_given_by_i <- UI_matrix[i,!is.na(UI_matrix[i,])]
@@ -51,9 +59,9 @@ update_assignment <- function(UI_matrix, C, assignment, prob_class, prob_rating)
     # we first compute all numerators in the formula for assignment[i,c]:
     for (c in 1:C){
       assignment[i,c] <- prob_class[c]
-      for (k in 1:length(movies_rated_by_i)){
+      for (idx in 1:length(movies_rated_by_i)){
         # still looking for a way to avoid this loop...
-        assignment[i,c] <- assignment[i,c] * prob_rating[c, movies_rated_by_i[[k]], ratings_given_by_i[[k]]]
+        assignment[i,c] <- assignment[i,c] * prob_rating[c, movies_rated_by_i[[idx]], ratings_given_by_i[[idx]] + k_fix]
       }
     }
     
@@ -77,23 +85,50 @@ update_prob_class <- function(UI_matrix, C, prob_class, assignment){
   return(prob_class)
 }
 
-update_prob_rating <- function(UI_matrix, C, prob_rating, assignment){
+update_prob_rating <- function(UI_matrix, C, prob_rating, assignment, possible_ratings){
   n_items <- dim(UI_matrix)[2]
+  k_fix <- ifelse(length(possible_ratings) == 2, 1, 0) # for MS, possible ratings are 0 or 1, but prob_rating is indexed starting 1, so shift if necessary
   for (c in 1:C){
     for (j in 1:n_items){
       users_who_rated_item_j <- which(!is.na(UI_matrix[,j]))
       denominator <- sum(assignment[users_who_rated_item_j,c])
       # warning: denominator can be = 0 ! (if prob_class got to 0 for class c at previous step)
-      # print(denominator)
-      for(k in 1:6){
+      # annoying issue: MS gives ratings 0 or 1, yet prob_rating is indexed starting from 1:
+      for (k in possible_ratings){
         if (denominator != 0){
           numerator <- sum((UI_matrix[users_who_rated_item_j,j] == k) * assignment[users_who_rated_item_j,c])
-          prob_rating[c,j,k] <- numerator/denominator
+          prob_rating[c,j,k + k_fix] <- numerator/denominator
         } else{
-          prob_rating[c,j,k] <- 0
+          prob_rating[c,j,k + k_fix] <- 0
         }
       }
     }
     return(prob_rating)
+  }
+}
+
+
+# Check parameters:
+em_check <- function(assignment, prob_class, prob_rating){
+  for (i in 1:dim(assignment)[1]){
+    # stopifnot(abs(sum(assignment_movies[i,]) - 1) < 0.001)
+    if (abs(sum(assignment[i,]) - 1) > 0.001){
+      print(paste0("Probabilities don't sum to 1 for assignment index i=", i))
+    }
+  }
+  
+  if (abs(sum(prob_class) - 1) > 0.001){
+    print("Probabilities don't sum to 1 for prob_class")
+  }
+  # stopifnot(abs(sum(prob_class) - 1) < 0.001)
+  
+  for (c in 1:dim(prob_rating)[1]){
+    for (j in 1:dim(prob_rating)[2]){
+      # stopifnot(abs(sum(prob_rating[c,j,]) - 1) < 0.001)
+      if (abs(sum(prob_rating[c,j,]) - 1) > 0.001){
+        print(paste0("Probabilities don't sum to 1 for prob_rating indices c,j=", c,",", j))
+        print(c(c,j))
+      }
+    }
   }
 }
--- lib/model_based_predictions.R
+++ lib/model_based_predictions.R
@@ -1,20 +1,27 @@
 # Functions to be run after the EM algorithm estimated the parameters, to predict ratings
 
 # Better version:
-estimate_rating_item <- function(UI_matrix, j, prob_class, prob_rating, assignment){
+estimate_rating_item <- function(UI_matrix, j, prob_class, prob_rating, assignment, possible_ratings){
   n_users <- dim(UI_matrix)[1]
   # estimate all ratings for movie j at once
   ratings <- matrix(0, nrow=n_users)
-  for (k in 1:6){
-    ratings <- ratings + k*assignment%*%prob_rating[,j,k]
+  k_fix <- ifelse(length(possible_ratings) == 2, 1, 0) # for MS, possible ratings are 0 or 1, but prob_rating is indexed starting 1, so shift if necessary
+  for (k in possible_ratings){
+    ratings <- ratings + k*assignment%*%prob_rating[,j,k + k_fix]
   }
   return(ratings)
 }
 
-predict_all_v2 <- function(UI_matrix, prob_class, prob_rating, assignment){
+predict_all_v2 <- function(UI_matrix, prob_class, prob_rating, assignment, dataset='movies'){
   n_items <- dim(UI_matrix)[2]
+  if (dataset=='movies'){
+    possible_ratings = (1:6)
+  } else if (dataset=='MS'){
+    possible_ratings = (0:1)
+  }
+
   for (j in 1:n_items){
-    UI_matrix[,j] <- estimate_rating_item(UI_matrix, j, prob_class, prob_rating, assignment)
+    UI_matrix[,j] <- estimate_rating_item(UI_matrix, j, prob_class, prob_rating, assignment, possible_ratings)
   }
   return(UI_matrix)
 }
@@ -22,31 +29,38 @@ predict_all_v2 <- function(UI_matrix, prob_class, prob_rating, assignment){
 # ###############################
 
 # Initial version:
-estimate_rating_user_item <- function(C, i, j, prob_class, prob_rating, assignment){
+estimate_rating_user_item <- function(C, i, j, prob_class, prob_rating, assignment, possible_ratings){
   # estimate rating of user i for item j, given learned parameters
   # Step 1: compute probabilities for each rating
   probs <- matrix(0, nrow=6)
-  for (k in 1:6){
+  k_fix <- ifelse(length(possible_ratings) == 2, 1, 0) # for MS, possible ratings are 0 or 1, but prob_rating is indexed starting 1, so shift if necessary
+  for (k in possible_ratings){
     for (c in 1:C){
-      probs[k] = probs[k] + assignment[i, c] * prob_rating[c, j, k]
+      probs[k + k_fix] = probs[k + k_fix] + assignment[i, c] * prob_rating[c, j, k + k_fix]
     }
   }
   # Step 2: compute expectation
   expected_rating <- 0
-  for (k in 1:6){
-    expected_rating <- expected_rating + k*probs[k]
+  for (k in possible_ratings){
+    expected_rating <- expected_rating + k*probs[k + k_fix]
   }
   return(expected_rating)
 }
 
-predict_all_slow <- function(UI_matrix, C, prob_class, prob_rating, assignment){
+predict_all_slow <- function(UI_matrix, C, prob_class, prob_rating, assignment, dataset='movies'){
   n_users <- dim(UI_matrix)[1]
   n_items <- dim(UI_matrix)[2]
+  if (dataset=='movies'){
+    possible_ratings = (1:6)
+  } else if (dataset=='MS'){
+    possible_ratings = (0:1)
+  }
+  
   # fills in UI_matrix and returns full matrix
   for (i in 1:n_users){
     for (j in 1:n_items){
       if (is.na(UI_matrix[i,j])){
-        UI_matrix[i,j] <- estimate_rating_user_item(C, i, j, prob_class, prob_rating, assignment)
+        UI_matrix[i,j] <- estimate_rating_user_item(C, i, j, prob_class, prob_rating, assignment, possible_ratings)
       }
     }
   }
